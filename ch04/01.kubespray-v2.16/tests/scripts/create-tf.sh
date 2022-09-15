289e301a617a7cc632caf9f86e9e401512579ce1
erdia22 <erdia22@gmail.com>
2022-09-15 09:12:46 +0900

    remov old directory

--- ch04/01.kubespray-v2.16/tests/scripts/create-tf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-cd ..
-terraform apply -auto-approve -parallelism=1 "contrib/terraform/$PROVIDER"
--- ch04/01.kubespray-v2.16/tests/scripts/delete-tf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-cd ..
-terraform destroy -auto-approve "contrib/terraform/$PROVIDER"
